da94180a29b77dfe652f025a32f18c5d94b5e48c
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-03 12:35:55 -0400

    aged brie tests passing

--- src/js/hard.js
+++ src/js/hard.js
@@ -6,23 +6,43 @@ class GildedRose {
   }
 
   tick () {
-    this.normal();
+    if (this.name === "Aged Brie"){
+      this.agedBrie();
+    } else {
+      this.normal();
+    }
 
   }
 
 
   normal () {
-      this.sellIn=this.sellIn-1;
-      if (this.quality>0){
-        if (this.sellIn<0){
-          this.quality = this.quality-2;
-        } else {
-          this.quality = this.quality-1;
-        }
+    this.sellIn=this.sellIn-1;
+    if (this.quality>0){
+      if (this.sellIn<0){
+        var qualityChange = -2;
+      } else {
+        var qualityChange = -1;
       }
+      this.quality=this.quality + qualityChange;
     }
+  }
 
-    
+  agedBrie(){
+    this.sellIn=this.sellIn-1;
+    if (this.sellIn<0){
+        var qualityChange = 2;
+      } else {
+        var qualityChange = 1;
+      }
+
+      if ((this.quality + qualityChange)>50){
+        this.quality = 50;
+      } else {
+        this.quality=this.quality + qualityChange;
+      }
+  }
+
+
 }
 
 var foo = new GildedRose();
